d4a4be5e8f3b29860fc75476a4b1837fcf800cb9
Fernand0-jf <142752157+Fernand0-jf@users.noreply.github.com>
2023-12-06 13:29:35 -0300

    Update biblioteca.hs

--- biblioteca.hs
+++ biblioteca.hs
@@ -1,1 +1,40 @@
 "# biblioteca_haskell" 
+import Data.List
+import Data.Char
+
+type Pessoa = String
+type Livro = String
+type Emprestimo = (Pessoa, Livro)
+type Emprestimos = [Emprestimo]
+type Situacao = String
+type Quantidade = Int
+
+
+
+
+emprestadoPorPessoa :: Pessoa -> Emprestimos -> [Livro] 
+emprestadoPorPessoa pes liv = [ a | (a,b)<- liv, a==pes ]
+
+livroPorPessoa :: Livro -> Emprestimos -> [Pessoa]
+livroPorPessoa liv pes = [ b| (a,b) <- pes, b == liv]
+
+situacaoLivro :: Livro -> Emprestimos -> Situacao
+situacaoLivro liv sit 
+  |null (livroPorPessoa liv sit)  = "Nao esta emprestado"
+  |otherwise                      = "Esta emprestado"
+
+
+qtddLivrosP :: Pessoa -> Emprestimos -> Quantidade
+qtddLivrosP pes ms = sum ( [ 1 | (a,b) <- ms, a == pes ] )
+
+adicionarEmprestimo :: Emprestimo -> Emprestimos -> Emprestimos
+adicionarEmprestimo emp emps = emp:emps
+
+retirarEmprestimo :: Emprestimo -> Emprestimos -> Situacao
+retirarEmprestimo emp emps 
+  |bancoDeDados == emps = "nao esta no banco de dados"
+  |otherwise            = "ja foi realizada sua devolucao"
+
+  where  
+    bancoDeDados :: Emprestimos
+    bancoDeDados = [(a,b) | (a,b) <- emps , fst emp/=a]
